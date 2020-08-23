7d74b06111bc534d4e9be91ed88308dac5a6d9be
DataSebi <sebatian_weber@posteo.de>
2020-08-23 19:19:40 +0200

    Added Enemy Movement

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,18 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Dynamic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public List<GameObject> waypoints;
+    public int currentTargetWaypointIndex;
+    public float movementSpeed = 5.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentTargetWaypointIndex = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        float movementStep = movementSpeed * Time.deltaTime;
+        GameObject currentTargetWaypoint = waypoints[currentTargetWaypointIndex];
+
+        float distance = Vector3.Distance(transform.position, currentTargetWaypoint.transform.position);
+        if (distance <= 0)
+        {
+            currentTargetWaypointIndex += 1;
+            currentTargetWaypoint = waypoints[currentTargetWaypointIndex];
+        }
+        //move enemy
+        transform.position = Vector3.MoveTowards(transform.position, currentTargetWaypoint.transform.position, movementStep);
+        if (currentTargetWaypoint.CompareTag("FinalWaypoint"))
+        {
+            Debug.Log("Remove One Health");
+            Destroy(gameObject);
+        }
     }
+
+    
 }
