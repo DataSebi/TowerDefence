0b0414f7551a14ac76991ea844caf3579f076148
DataSebi <sebatian_weber@posteo.de>
2020-08-23 15:32:58 +0200

    Added Castle and Game Manager

--- Assets/Scripts/BuildSocket.cs
+++ Assets/Scripts/BuildSocket.cs
@@ -1,30 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class BuildSocket : MonoBehaviour
 {
     public GameObject turret;
-    public GameObject buildScreen;
-    private Button buildButton;
+    private GameManager gameManger;
 
     // Start is called before the first frame update
     void Start()
     {
-        buildButton = buildScreen.GetComponentInChildren<Button>();
-        buildButton.onClick.AddListener(Build);
+        gameManger = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
     private void OnMouseDown()
     {
-        buildScreen.SetActive(true);
-    }
-
-    public void Build()
-    {
-        Instantiate(turret, transform.position, transform.rotation);
-        gameObject.SetActive(false);
-        buildScreen.SetActive(false);
+        gameManger.SelectBuildSocket(this);
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // ressources
+    private BuildSocket currentBuildSocketSelected;
+
+    public GameObject buildScreen;
+    private Button buildButton;
+
+    void Start()
+    {
+        buildButton = buildScreen.GetComponentInChildren<Button>();
+    }
+
+    public void SelectBuildSocket(BuildSocket turretSocket)
+    {
+        //active Build Screen
+        currentBuildSocketSelected = turretSocket;
+        buildScreen.SetActive(true);
+        buildButton.onClick.AddListener(BuildTurret);
+    }
+
+    public void BuildTurret()
+    {
+        //build on currentSelectedTurretSocket
+        Instantiate(currentBuildSocketSelected.turret, currentBuildSocketSelected.transform.position, currentBuildSocketSelected.transform.rotation);
+        gameObject.SetActive(false);
+        buildScreen.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
